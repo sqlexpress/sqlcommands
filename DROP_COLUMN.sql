5baf4daa5f2998e5f1472841b6887369232df873
Expert Modassir <145168476+expertmodassir@users.noreply.github.com>
2023-09-17 20:44:54 +0530

    commit on 17-09-2023

---
+++ DROP_COLUMN.sql
@@ -0,0 +1,10 @@
+-- How to DROP single column
+ALTER TABLE `table_name` DROP `column_name`;
+
+-- How to DROP multiple columns
+ALTER TABLE `table_name` DROP `column_1`,
+DROP `column_2`,
+DROP `column_3`,
+DROP `column_4`,
+DROP `column_5`,
+DROP `column_6`;
